508f61f3ffe72d3114f5328bc3d408b7cb1c34ba
katie-sun <112536538+katie-sun@users.noreply.github.com>
2022-11-27 20:54:41 -0500

    yesssssss working

--- final-project-draft-copy/colors.js
+++ final-project-draft-copy/colors.js
@@ -34,20 +34,20 @@ class Roll {
 
 */
 
-function updateElement() {
-  const imageElement = document.querySelector("#meshgradient");
-  const hexElement = document.querySelector("#hexcode");
-  const counterElement = document.querySelector('#counternumber');
+// function updateElement() {
+//   const imageElement = document.querySelector("#meshgradient");
+//   const hexElement = document.querySelector("#hexcode");
+//   const counterElement = document.querySelector('#counternumber');
 
-  // imageElement.src = "Assets/fp4/" + this.imageFile;
-  imageElement.src = "Assets/fp4/black-pantone.jpg";
-  // hexElement.innerText = this.hexCode;
-  hexElement.innerHTML = "#011B10";
+//   // imageElement.src = "Assets/fp4/" + this.imageFile;
+//   imageElement.src = "Assets/fp4/black-pantone.jpg";
+//   // hexElement.innerText = this.hexCode;
+//   hexElement.innerHTML = "#011B10";
 
-  // counterElement.innerText = this.dayCounter;
-  counterElement.innerHTML = 3;
+//   // counterElement.innerText = this.dayCounter;
+//   counterElement.innerHTML = 3;
 
-}
+// }
 
 //for each element if Object.keys(cardInfo[1]) = the ID, then run using Object.values
 
@@ -65,10 +65,28 @@ function updateElement() {
 // }
 
 resource: https://stackoverflow.com/questions/28444457/get-id-of-element-clicked
+
 document.addEventListener('click', function (e) {
   let clickedItem = e.target.id;
-  const testing = document.querySelector('#hexcode');
-  testing.innerHTML = clickedItem;
+
+
+  for (let i = 0; i < cardInfo.length; i++) {
+    if (cardInfo[i].colorID == clickedItem) {
+      const hexElement = document.querySelector('#hexcode');
+      hexElement.innerHTML = cardInfo[i].hexCode;
+
+      const counterElement = document.querySelector('#counternumber');
+      counterElement.innerHTML = cardInfo[i].dayCounter;
+
+      const imageElement = document.querySelector('#meshgradient');
+      imageElement.src = "Assets/fp4/" + cardInfo[i].imageFile;
+
+      const descriptorElement = document.querySelector('#colordescription');
+      descriptorElement.innerHTML = cardInfo[i].colorID;
+
+    }
+
+  }
 
   console.log(clickedItem);
 }, false);
@@ -86,18 +104,18 @@ document.addEventListener('click', function (e) {
 // }
 
 
-function searchArray(clickedItem) {
+// function searchArray(clickedItem) {
 
-  const test = clickedItem.value;
-  console.log(test);
+//   const test = clickedItem.value;
+//   console.log(test);
 
-  for (let i = 0; i < cardInfo.length; i++) {
-    if (cardInfo[i].colorID == clickedItem.value) {
-      // return cardInfo[i];
-      console.log(cardInfo[i]);
-    }
-  }
-}
+//   for (let i = 0; i < cardInfo.length; i++) {
+//     if (cardInfo[i].colorID == clickedItem.value) {
+//       // return cardInfo[i];
+//       console.log(cardInfo[i]);
+//     }
+//   }
+// }
 
 // const resulttest = searchArray("darkgreen", cardInfo);
 // console.log(resulttest);
@@ -160,239 +178,3 @@ function populateHtml() {
 // }
 
 
-
-
-
-function changeColor() {
-  document.getElementById("meshgradient").style.background = "011B10";
-  document.getElementById("hexcode").innerHTML = "011B10";
-  document.getElementById("counternumber").innerHTML = "15";
-}
-
-
-
-// const colorSet = new Set();
-
-
-// let colorBox = document.querySelector("colorgrid");
-// let newElement = document.createElement('div');
-// newElement.classList.add('colorbox');
-// colorBox.appendChild(newElement);
-
-
-//Parse the URL parameter and store the current color type as a variable
-
-
-// variable = chosenRoll (which is storing the current roll type as a variable)
-
-
-// Use the URL parameter to update page
-
-// const imageElement = document.querySelector("#meshgradient");
-// const hexElement = document.querySelector("#hexcode");
-// const counterElement = document.querySelector("#counternumber");
-
-// imageElement.src = color.imageFile;
-// hexElement.innerHTML = color.hexCode;
-// counterElement.innerHTML = color.day;
-
-
-
-
-// console.log(newElement);
-
-
-
-// for (let i = 0; i < colorInfo.length; i++) {
-//   // create HTML element
-//   const option = document.createElement('option');
-//   // access value in array
-//   option.value = colorInfo[i];
-//   // update blank HTML w/type of glaze 
-//   option.innerHTML = colorInfo[i];
-//   // append to HTML element
-//   colorBox.appendChild(option);
-// }
-
-
-// const colorBox = document.createElement("p");
-// const node = document.createTextNode("test");
-// colorBox.appendChild(node);
-
-// const element = document.getElementById("colorgrid");
-// element.appendChild(colorBox);
-
-
-//the 'new' keyword
-//creates a new empty object {}
-//sets the value of 'this' [the new object that new created] to be the new empty project
-// calls the constructor method to make the object + add properties to object
-
-
-
-
-//glazes and prices array
-let glazeInfo = [
-  { glazingOptions: "Keep Original", glazingPrice: 0.00 },
-  { glazingOptions: "Sugar Milk", glazingPrice: 0.00 },
-  { glazingOptions: "Vanilla Milk", glazingPrice: 0.50 },
-  { glazingOptions: "Double Chocolate", glazingPrice: 1.50 }
-]
-
-//pack sizes and prices array
-let packInfo = [
-  { packSize: "1", packPrice: 1 },
-  { packSize: "3", packPrice: 3 },
-  { packSize: "6", packPrice: 5 },
-  { packSize: "12", packPrice: 10 },
-]
-
-
-
-//populate DOM w/objects 
-
-
-//update option using for loop w/.js
-
-
-// When the page loads, find the Glaze select element.
-
-
-// When the page loads, find the Pack Size select element.
-let selectPackSize = document.querySelector("#packsizes");
-
-if (document.URL.includes("productdetail.html")) {
-
-  for (let i = 0; i < packInfo.length; i++) {
-    // create HTML element
-    const option = document.createElement('option');
-    // access value at array 
-    option.value = packInfo[i].packSize;
-    // update blank HTML w/pack size
-    option.innerHTML = packInfo[i].packSize;
-    // append to pack size dropdown
-    selectPackSize.appendChild(option);
-  }
-}
-
-// Set constants + variables
-let basePrice = 2.49;
-let glazingPrice = 0.00;
-let packPrice = 1;
-
-//function to trigger price update based on user selections in the drop down
-function glazingChange(element) {
-  // get value of selected glazing option
-  const priceChange = element.value;
-
-  //retrieve glaze object corresponding to the selected option
-
-  //loop through array
-  for (let i = 0; i < glazeInfo.length; i++) {
-
-    //find out if item in dropdown selection equals item in array
-    if (glazeInfo[i].glazingOptions == element.value) {
-
-      //if so, set glazingPrice variable to price addition
-      glazingPrice = glazeInfo[i].glazingPrice;
-    }
-  }
-
-  //retrieve pack size object corresponding to the selected option
-  for (let i = 0; i < packInfo.length; i++) {
-    if (packInfo[i].packSize == element.value) {
-      packPrice = packInfo[i].packPrice;
-    }
-  }
-
-  //calculate total price based on glaze selection + quantity
-  let totalPrice = ((basePrice + glazingPrice) * packPrice);
-
-  //update total price according to the change
-  document.getElementById("totalprice").innerHTML = ('$' + totalPrice.toFixed(2));
-
-
-}
-
-cart = [];
-
-
-
-
-//attempt 3 modeled after pui note
-//function to add new rolls to cart set
-function addToCart() {
-
-
-  //Function to calculate the total price of an item based on adjustments
-  function calculatePrice(basePrice, glazingPrice, packPrice) {
-    return (basePrice + glazingPrice) * packPrice;
-  }
-
-
-  //function to replace hardcoding the rolls (to create new Roll objects)
-  function createCartRoll(rollType, rollGlazing, packSize, rollPrice) {
-    const newRoll = new Roll(rollType, rollGlazing, packSize, rollPrice);
-    cartSet.add(newRoll);
-    return newRoll;
-  }
-
-  let totalPrice = 0;
-
-  if (document.URL.includes("shoppingcart.html")) {
-    //function to populate cart with items based on template
-    function fillCart(item) {
-      //grab reference to cart item template
-      const template = document.querySelector('#cartitems-template');
-
-      //get content inside template and copy it using a clone method
-      let clone = template.content.cloneNode(true);
-
-      //get rid of the fragment
-      let cartElement = clone.querySelector('.cinnamonbun');
-
-      //grab reference to div that will hold all cart items
-      let cartGallery = document.querySelector('.sc-gallery');
-
-      //Appends appropriate DOM elements to shopping cart page
-      cartElement.querySelector("#sc_Title").innerText = item.type + " Cinnamon Roll";
-      cartElement.querySelector("#sc_Glazing").innerText = "Glazing: " + item.glazing;
-      cartElement.querySelector("#sc_PackSize").innerText = "Pack Size: " + item.size;
-      cartElement.querySelector(".donut-flavor-static").src = './Assets/hw-1-assets/' + item.type.toLowerCase() + "-cinnamon-roll.jpeg";
-      cartElement.querySelector("#sc_individualitemprice").innerText = '$' + (item.calculatedPrice).toFixed(2);
-
-      //calculate total price of cart (within the loop)
-      totalPrice = totalPrice + item.calculatedPrice;
-      document.querySelector("#sc_totalprice").innerText = "$ " + totalPrice.toFixed(2);
-
-      //add our newly created elements
-      cartGallery.append(cartElement);
-
-
-      //remove cart elements
-      let deleteButton = cartElement.querySelector('#deletebun');
-      deleteButton.onclick = (() => {
-        cartElement.remove();
-
-        let totalPrice = parseFloat(document.querySelector("#sc_totalprice").innerText.slice(2));
-        let rollPrice = parseFloat(cartElement.querySelector("#sc_individualitemprice").innerText.slice(1)).toFixed(2);
-        //change total price each time a cart element is removed
-        let newTotalPrice = totalPrice - rollPrice;
-
-        document.querySelector("#sc_totalprice").innerText = "$ " + newTotalPrice.toFixed(2);
-        //delete item from cart
-        cartSet.delete(item);
-
-        //update local storage when each item is removed + log in console
-        saveToLocalStorage();
-        console.log(cartSet);
-
-      })
-    }
-    for (item of cartSet) {
-      fillCart(item);
-
-    }
-
-  }
-}
